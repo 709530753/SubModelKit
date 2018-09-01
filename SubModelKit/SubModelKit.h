48222d78410628593048229989d73a7b51745c49
myxc <70953073@qq.com>
2018-09-01 22:18:08 +0800

    commit

---
+++ SubModelKit/SubModelKit.h
@@ -0,0 +1,19 @@
+//
+//  SubModelKit.h
+//  SubModelKit
+//
+//  Created by myxc on 2018/9/1.
+//  Copyright © 2018 myxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SubModelKit.
+FOUNDATION_EXPORT double SubModelKitVersionNumber;
+
+//! Project version string for SubModelKit.
+FOUNDATION_EXPORT const unsigned char SubModelKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SubModelKit/PublicHeader.h>
+
+
